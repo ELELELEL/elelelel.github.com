eea0819f845578294a2a15803c2c34ff45a3db15
ELEL <gamemasteroverpower@gmail.com>
2015-12-13 22:44:45 -0800

    Update main.js

--- stylesheets/main.js
+++ stylesheets/main.js
@@ -1,10 +1,10 @@
 var storage = window.localStorage;
 if (!storage.getItem("shengming")) storage.setItem("shengming",100);
 shengming = parseInt(storage.getItem("shengming"));//必须格式转换
-document.getElementById("a1").innerHTML = "生命值" + shengming;
+document.getElementById("shengmingzhi").innerHTML = "生命值" + shengming;
 showStorage();
 
 setInterval(	function() 
 	{ 
 	  document.getElementById("shengmingzhi").innerHTML = "生命值" + shengming;
-	} , 1000);
+	} , 100);
