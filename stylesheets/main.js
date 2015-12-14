235bae57ec9189433d3b7612129207a29cdc5fe5
ELEL <gamemasteroverpower@gmail.com>
2015-12-13 22:42:13 -0800

    Update main.js

--- stylesheets/main.js
+++ stylesheets/main.js
@@ -3,3 +3,8 @@ if (!storage.getItem("shengming")) storage.setItem("shengming",100);
 shengming = parseInt(storage.getItem("shengming"));//必须格式转换
 document.getElementById("a1").innerHTML = "生命值" + shengming;
 showStorage();
+
+setInterval(	function() 
+	{ 
+	  document.getElementById("a1").innerHTML = "生命值" + shengming;
+	} , 1000);
